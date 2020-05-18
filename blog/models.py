ba7c6603550c4a54a231c043237a72231acef2f1
Murat0901 <menzilcim@gmail.com>
2020-05-18 19:08:59 +0300

    post modify

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 class BlogPost(models.Model):
     BlogTitle = models.CharField(max_length=100, verbose_name="Başlık")
-    BlogContent = models.TextField(verbose_name="İçerik")
+    BlogContent = RichTextField(verbose_name="İçerik")
     BlogDate = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
